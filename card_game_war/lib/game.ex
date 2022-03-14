285dd452aebdf129e17b435afc076190e634b60d
Caleb J. Acosta <keyboardkiller.gk@gmail.com>
2022-03-14 10:54:34 -0600

    CardGameWar Solution

--- card_game_war/lib/game.ex
+++ card_game_war/lib/game.ex
@@ -1,13 +1,64 @@
 defmodule CardGameWar.Game do
 
+  defmodule Card do
+    defstruct [:rank, :suit]
+  end
   # feel free to use these cards or use your own data structure"
   def suits, do: [:spade, :club, :diamond, :heart]
   def ranks, do: [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
   def cards do
-    for suit <- suits,
-        rank <- ranks do
-      {suit, rank}
+    for suit <- suits(),
+        rank <- ranks() do
+      %Card{suit: suit, rank: rank}
+    end
+  end
+  def get_higher(card_1, card_2) do
+    index_1 = Enum.find_index(ranks(), &(&1 == card_1.rank))
+    index_2 = Enum.find_index(ranks(), &(&1 == card_2.rank))
+    cond do
+      index_1 > index_2 ->
+        card_1
+      index_1 < index_2 ->
+        card_2
+      true -> 
+         cond do
+           Enum.find_index(suits(), &(&1 == card_1.suit)) > Enum.find_index(suits(), &(&1 == card_2.suit)) ->
+             card_1
+           true -> 
+             card_2
+         end
     end
   end
 
+  def play() do
+    {deck_player_1, deck_player_2} = Enum.shuffle(CardGameWar.Game.cards) |> Enum.split(26)
+    play(26, 26, deck_player_1, deck_player_2) 
+  end
+
+  def play(0, _, _, _) do
+    "Player 2 wins"
+  end
+  def play(_, 0, _, _) do
+    "Player 1 wins"
+  end
+  def play(_, _, deck_player_1, deck_player_2) do
+    [card_player_1 | deck_player_1] = deck_player_1
+    [card_player_2 | deck_player_2] = deck_player_2
+    IO.inspect("Player 1 plays: " <> inspect(card_player_1.rank) <> " " <> inspect(card_player_1.suit))
+    IO.inspect("Player 2 plays: " <> inspect(card_player_2.rank) <> " " <> inspect(card_player_2.suit))
+    #IO.inspect(length(deck_player_1))
+    #IO.inspect(length(deck_player_2))
+    cond do
+      get_higher(card_player_1, card_player_2) == card_player_1 ->
+        IO.inspect("Player 1 won round")
+        deck_player_1 = [card_player_1 | Enum.reverse deck_player_1]
+        deck_player_1 = [card_player_2 | deck_player_1]
+        play(length(deck_player_1), length(deck_player_2), Enum.reverse(deck_player_1), deck_player_2)
+      true ->
+        IO.inspect("Player 2 won round")
+        deck_player_2 = [card_player_2 | Enum.reverse deck_player_2]
+        deck_player_2 = [card_player_1 | deck_player_2]
+        play(length(deck_player_1), length(deck_player_2), deck_player_1, Enum.reverse(deck_player_2))
+    end
+  end
 end
--- card_game_war/test/game_test.exs
+++ card_game_war/test/game_test.exs
@@ -1,36 +1,48 @@
 defmodule CardGameWar.GameTest do
   use ExUnit.Case
-
+  alias CardGameWar.Game.Card
   test "the highest rank wins the cards in the round" do
-    flunk("not implemented")
+    assert CardGameWar.Game.get_higher(%Card{suit: :diamond, rank: 8}, %Card{suit: :spade, rank: 2}) ==
+      %Card{suit: :diamond, rank: 8}
   end
 
   test "queens are higher rank than jacks" do
-    flunk("not implemented")
+    assert CardGameWar.Game.get_higher(%Card{suit: :diamond, rank: :queen}, %Card{suit: :spade, rank: :jack}) ==
+      %Card{suit: :diamond, rank: :queen}
   end
 
   test "kings are higher rank than queens" do
-    flunk("not implemented")
+    assert CardGameWar.Game.get_higher(%Card{suit: :heart, rank: :queen}, %Card{suit: :club, rank: :king}) ==
+      %Card{suit: :club, rank: :king}
   end
 
   test "aces are higher rank than kings" do
-    flunk("not implemented")
+    assert CardGameWar.Game.get_higher(%Card{suit: :spades, rank: :ace}, %Card{suit: :club, rank: :king}) ==
+      %Card{suit: :spades, rank: :ace}
   end
+  #The only thing you see, you know it's gonna be the Ace of Spades
+  #The Ace of Spades
 
   test "if the ranks are equal, clubs beat spades" do
-    flunk("not implemented")
+    assert CardGameWar.Game.get_higher(%Card{suit: :club, rank: 8}, %Card{suit: :spade, rank: 8}) ==
+      %Card{suit: :club, rank: 8}
+
   end
 
   test "if the ranks are equal, diamonds beat clubs" do
-    flunk("not implemented")
+    assert CardGameWar.Game.get_higher(%Card{suit: :club, rank: :jack}, %Card{suit: :diamond, rank: :jack}) ==
+      %Card{suit: :diamond, rank: :jack}
+
   end
 
   test "if the ranks are equal, hearts beat diamonds" do
-    flunk("not implemented")
+    assert CardGameWar.Game.get_higher(%Card{suit: :heart, rank: 8}, %Card{suit: :diamond, rank: 8}) ==
+      %Card{suit: :heart, rank: 8}
   end
 
   test "the player loses when they run out of cards" do
-    flunk("not implemented")
+    assert CardGameWar.Game.play(52, 0, :deck_player_1, :deck_player_2) == "Player 1 wins"
+    assert CardGameWar.Game.play(0, 52, :deck_player_1, :deck_player_2) == "Player 2 wins"
   end
 
 end
